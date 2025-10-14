bec6fea90ffadddf999b41f62bb2eda16db84fe8
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-14 23:02:16 +0800

    feat: configure react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
-
 // @ts-check
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
