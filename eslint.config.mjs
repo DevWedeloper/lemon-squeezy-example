7d58c32bebbb6544d3afdc989ecaad4e3889c2a4
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-14 22:59:09 +0800

    feat: configure antfu eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import antfu from '@antfu/eslint-config'
+import tsParser from '@typescript-eslint/parser'
+import eslintParserAstro from 'astro-eslint-parser'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
+
+export default antfu(
+  {
+    formatters: true,
+    astro: true,
+    react: true,
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.jsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: eslintParserAstro,
+    },
+  },
+  {
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+      'better-tailwindcss/enforce-consistent-line-wrapping': ['off'],
+      'better-tailwindcss/no-unregistered-classes': ['off'],
+    },
+  },
+)
