e7d950d3753180897fd1983d68f6a4608acf7a83
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-15 22:45:30 +0800

    feat: add signing secret to env

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 
 const EnvSchema = z.object({
   LEMONSQUEEZY_API_KEY: z.string(),
+  SIGNING_SECRET: z.string(),
 })
 
 const { data: env, error } = EnvSchema.safeParse(import.meta.env)
